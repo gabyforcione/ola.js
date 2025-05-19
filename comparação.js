b65cb2b351d12be6dfbd9766cc419082d4b1979e
gabyforcione <gabriellyforcione@colegiopaulodetarso.com.br>
2025-05-19 16:21:57 +0000

    oi

---
+++ comparação.js
@@ -0,0 +1,12 @@
+function ehMaior(a,b){
+    if (a > b) {
+        return true
+    } else {
+     return false 
+    }
+} 
+
+
+console.log(ehMaior(10, 5));   // true
+console.log(ehMaior(2, 8));    // false
+console.log(ehMaior(7, 7));    // false
